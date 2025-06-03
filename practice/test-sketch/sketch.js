af48348149f3686ae46a10f0088e1c4cff2f1631
mckee-401 <aoiirietakumi@gmail.com>
2025-06-03 23:54:37 +0900

    Initial commit

---
+++ practice/test-sketch/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+  background(240);
+}
+
+function draw() {
+  fill(100, 150, 255);
+  ellipse(width / 2, height / 2, 100, 100);
+}
+
+//正しいURLはhttp://localhost:5555/test-sketch/
